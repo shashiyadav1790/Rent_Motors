f77ecc913050e0c3c542b129bdab888ffa06a051
shashi yadav <shashiyadav1790@gmail.com>
2025-02-27 14:12:03 +0530

    initial commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {},
-  },
   esbuild: {
-    jsxInject: `"use client";`
+    jsxInject: `import React from 'react';`, // Remove "use client" injection
   }
-})
+});
